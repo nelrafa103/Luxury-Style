41f365df2c2459ab30f744dffdfabec1c5c85ec5
nelrafa103 <60740045+nelrafa103@users.noreply.github.com>
2020-03-12 18:50:20 -0400

    Update style.js

--- js/style.js
+++ js/style.js
@@ -25,13 +25,141 @@ function myFunction(){
   document.getElementById("producto_8.2").src="images/CamisasTops/product-28.jpg";
   };
 ;
+function myFunction2(){
+  document.getElementById("producto_1.1").href="images/Vestidos/product-6.jpg";
+  document.getElementById("producto_1.2").src="images/Vestidos/product-6.jpg"; 
 
+  document.getElementById("producto_2.1").href="images/Vestidos/product-11.jpg";
+  document.getElementById("producto_2.2").src="images/Vestidos/product-11.jpg";
 
+  document.getElementById("producto_3.1").href="images/Vestidos/product-31.jpg";
+  document.getElementById("producto_3.2").src="images/Vestidos/product-31.jpg";
 
+  document.getElementById("producto_4.1").href="images/Vestidos/product-7.jpg";
+  document.getElementById("producto_4.2").src="images/Vestidos/product-7.jpg";
 
+  document.getElementById("producto_5.1").href="images/Vestidos/product-13.jpg";
+  document.getElementById("producto_5.2").src="images/Vestidos/product-13.jpg";
 
-function myFunction9(){
-  var x = document.getElementById("producto_8.2")
+  document.getElementById("producto_6.1").href="images/Vestidos/product-2.jpg";
+  document.getElementById("producto_6.2").src="images/Vestidos/product-2.jpg";
+
+  document.getElementById("producto_7.1").href="images/Vestidos/product-5.jpg";
+  document.getElementById("producto_7.2").src="images/Vestidos/product-5.jpg";
+
+  document.getElementById("producto_8.1").href="images/Vestidos/product-30.jpg";
+  document.getElementById("producto_8.2").src="images/Vestidos/product-30.jpg";
+  };
+;
+
+function myFunction3(){
+  document.getElementById("producto_1.1").href="images/Faldas y Bermudas/product-16.jpg";
+  document.getElementById("producto_1.2").src="images/Faldas y Bermudas/product-16.jpg"; 
+ 
+  document.getElementById("producto_2.1").href="images/Faldas y Bermudas/product-20.jpg";
+  document.getElementById("producto_2.2").src="images/Faldas y Bermudas/product-20.jpg";
+
+  document.getElementById("producto_3.1").href="images/Faldas y Bermudas/product-19.jpg";
+  document.getElementById("producto_3.2").src="images/Faldas y Bermudas/product-19.jpg";
+
+  document.getElementById("producto_4.1").href="images/Faldas y Bermudas/product-18.jpg";
+  document.getElementById("producto_4.2").src="images/Faldas y Bermudas/product-18.jpg";
+
+  document.getElementById("producto_5.1").href="images/Faldas y Bermudas/product-17.jpg";
+  document.getElementById("producto_5.2").src="images/Faldas y Bermudas/product-17.jpg";
+ 
+  document.getElementById("producto_6.1").href="images/Faldas y Bermudas/product-35.jpg";
+  document.getElementById("producto_6.2").src="images/Faldas y Bermudas/product-35.jpg";
+
+  document.getElementById("producto_7.1").href="images/Faldas y Bermudas/product-36.jpg";
+  document.getElementById("producto_7.2").src="images/Faldas y Bermudas/product-36.jpg";
+  
+  document.getElementById("producto_8.1").href="images/Faldas y Bermudas/product-34.jpg";
+  document.getElementById("producto_8.2").src="images/Faldas y Bermudas/product-34.jpg";
+ 
+  };
+;
+
+function myFunction4(){
+  document.getElementById("producto_1.1").href="images/Chaquetas/product-1.jpg";
+  document.getElementById("producto_1.2").src="images/Chaquetas/product-1.jpg"; 
+  
+  document.getElementById("producto_2.1").href="images/Chaquetas/product-37.jpg";
+  document.getElementById("producto_2.2").src="images/Chaquetas/product-37.jpg";
+  
+  document.getElementById("producto_3.1").href="images/Chaquetas/product-38.jpg";
+  document.getElementById("producto_3.2").src="images/Chaquetas/product-38.jpg";
+ 
+  document.getElementById("producto_4.1").href="images/Chaquetas/product-40.jpg";
+  document.getElementById("producto_4.2").src="images/Chaquetas/product-40.jpg";
+  
+  document.getElementById("producto_5.1").href="images/Chaquetas/product-39.jpg";
+  document.getElementById("producto_5.2").src="images/Chaquetas/product-39.jpg";
+  
+  document.getElementById("producto_6.1").href="images/Chaquetas/product-42.jpg";
+  document.getElementById("producto_6.2").src="images/Chaquetas/product-42.jpg";
+ 
+  document.getElementById("producto_7.1").href="images/Chaquetas/product-41.jpg";
+  document.getElementById("producto_7.2").src="images/Chaquetas/product-41.jpg";
+ 
+  document.getElementById("producto_8.1").href="images/Chaquetas/product-43.jpg";
+  document.getElementById("producto_8.2").src="images/Chaquetas/product-43.jpg";
+  };
+;
+
+function myFunction5(){
+  document.getElementById("producto_1.1").href="images/Abrigos/product-43.jpg";
+  document.getElementById("producto_1.2").src="images/Abrigos/product-43.jpg"; 
+
+  document.getElementById("producto_2.1").href="images/Abrigos/product-44.jpg";
+  document.getElementById("producto_2.2").src="images/Abrigos/product-44.jpg";
+  
+  document.getElementById("producto_3.1").href="images/Abrigos/product-45.jpg";
+  document.getElementById("producto_3.2").src="images/Abrigos/product-45.jpg";
+ 
+  document.getElementById("producto_4.1").href="images/Abrigos/product-46.jpg";
+  document.getElementById("producto_4.2").src="images/Abrigos/product-46.jpg";
+  
+  document.getElementById("producto_5.1").href="images/Abrigos/product-47.jpg";
+  document.getElementById("producto_5.2").src="images/Abrigos/product-47.jpg";
+  
+  document.getElementById("producto_6.1").href="images/Abrigos/product-48.jpg";
+  document.getElementById("producto_6.2").src="images/Abrigos/product-48.jpg";
+ 
+  document.getElementById("producto_7.1").href="images/Abrigos/product-49.jpg";
+  document.getElementById("producto_7.2").src="images/Abrigos/product-49.jpg";
+
+  document.getElementById("producto_8.1").href="images/Abrigos/product-50.jpg";
+  document.getElementById("producto_8.2").src="images/Abrigos/product-50.jpg";
+ 
+  };
+;
+
+function myFunction6(){
+  document.getElementById("producto_1.1").href="images/Jeans/product-51.jpg";
+  document.getElementById("producto_1.2").src="images/Jeans/product-51.jpg"; 
+
+  document.getElementById("producto_2.1").href="images/Jeans/product-52.jpg";
+  document.getElementById("producto_2.2").src="images/Jeans/product-52.jpg";
+
+  document.getElementById("producto_3.1").href="images/Jeans/product-53.jpg";
+  document.getElementById("producto_3.2").src="images/Jeans/product-53.jpg";
+
+  document.getElementById("producto_4.1").href="images/Jeans/product-54.jpg";
+  document.getElementById("producto_4.2").src="images/Jeans/product-54.jpg";
+
+  document.getElementById("producto_5.1").href="images/Jeans/product-55.jpg";
+  document.getElementById("producto_5.2").src="images/Jeans/product-55.jpg";
+  
+  document.getElementById("producto_6.1").href="images/Jeans/product-56.jpg";
+  document.getElementById("producto_6.2").src="images/Jeans/product-56.jpg";
+
+  document.getElementById("producto_7.1").href="images/Jeans/product-57.jpg";
+  document.getElementById("producto_7.2").src="images/Jeans/product-57.jpg";
+ 
+  document.getElementById("producto_8.1").href="images/Jeans/product-58.jpg";
+  document.getElementById("producto_8.2").src="images/Jeans/product-58.jpg";
+  
   };
 ;
 let slider = document.querySelector(".slider-contenedor")
