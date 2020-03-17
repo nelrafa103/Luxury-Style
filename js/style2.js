c79e43ca3022995e0b9a2927c49c985ce6226ba9
nelrafa103 <60740045+nelrafa103@users.noreply.github.com>
2020-03-16 21:50:25 -0400

    Update style2.js

---
+++ js/style2.js
@@ -0,0 +1,95 @@
+function myFunction(){
+ document.getElementById("product_0").href = "images/Chaquetas/product-43.jpg"
+ document.getElementById("product_0_1").src = "images/Chaquetas/product-43.jpg"
+
+ document.getElementById("product_0_2").src = "images/Chaquetas/product-1.jpg"
+ document.getElementById("product_0_3").src = "images/Vestidos/product-2.jpg"
+ document.getElementById("product_0_4").src = "images/Vestidos/product-3.jpg"
+ document.getElementById("product_0_5").src = "images/Vestidos/product-13.jpg"
+
+ document.getElementById("product_1").innerHTML = "Chaqueta North Face"
+ document.getElementById("product_2").innerHTML = "Vestido Karen Miller"
+ document.getElementById("product_3").innerHTML = "Vestido Tommy Hilfige"
+ document.getElementById("product_4").innerHTML = "Vestido Tintoretto"
+
+
+ document.getElementById("price_2").innerHTML = "RD$ 3750"
+ document.getElementById("price_0").innerHTML = "RD$ 6000"
+ document.getElementById("price_1").innerHTML = "RD$ 5250"
+ document.getElementById("price_3").innerHTML = "RD$ 3900"
+ document.getElementById("price_4").innerHTML = "RD$ 5900"
+ };
+;
+
+function myFunction2(){
+    document.getElementById("product_0").href = "images/Vestidos/product-2.jpg"
+    document.getElementById("product_0_1").src = "images/Vestidos/product-2.jpg"
+
+    document.getElementById("product_0_3").src = "images/Chaquetas/product-1.jpg"
+    document.getElementById("product_0_2").src = "images/Chaquetas/product-43.jpg"
+    document.getElementById("product_0_4").src = "images/Vestidos/product-3.jpg"
+    document.getElementById("product_0_5").src = "images/Vestidos/product-13.jpg"
+
+    document.getElementById("product_2").innerHTML = "Chaqueta North Face"
+    document.getElementById("product_1").innerHTML = "Chaqueta Tommy Hilfiger"
+    document.getElementById("product_3").innerHTML = "Vestido Tommy Hilfige"
+    document.getElementById("product_4").innerHTML = "Vestido Tintoretto"
+
+    document.getElementById("price_0").innerHTML = "RD$ 7000"
+    document.getElementById("price_1").innerHTML = "RD$ 6050"
+    document.getElementById("price_2").innerHTML = "RD$ 5250"
+    document.getElementById("price_3").innerHTML = "RD$ 3900"
+    document.getElementById("price_4").innerHTML = "RD$ 5900"
+    };
+   ;
+
+   function myFunction3(){
+    document.getElementById("product_0").href = "images/Vestidos/product-3.jpg"
+    document.getElementById("product_0_1").src = "images/Vestidos/product-3.jpg"
+  
+
+    document.getElementById("product_0_2").src = "images/Chaquetas/product-43.jpg"
+    document.getElementById("product_0_3").src = "images/Vestidos/product-2.jpg"
+    document.getElementById("product_0_4").src = "images/Chaquetas/product-1.jpg"
+    document.getElementById("product_0_5").src = "images/Vestidos/product-13.jpg"
+
+
+    document.getElementById("product_3").innerHTML = "Chaqueta North Face"
+    document.getElementById("product_2").innerHTML = "Vestido Karen Miller"
+    document.getElementById("product_1").innerHTML = "Chaqueta Tommy Hilfiger"
+    document.getElementById("product_4").innerHTML = "Vestido Tintoretto"
+
+    document.getElementById("price_2").innerHTML = "RD$ 3750"
+    document.getElementById("price_0").innerHTML = "RD$ 7000"
+    document.getElementById("price_1").innerHTML = "RD$ 6050"
+    document.getElementById("price_3").innerHTML = "RD$ 5250"
+    document.getElementById("price_4").innerHTML = "RD$ 5900"
+    
+    };
+   ;
+
+   
+   function myFunction4(){
+    document.getElementById("product_0").href = "images/Vestidos/product-13.jpg"
+    document.getElementById("product_0_1").src = "images/Vestidos/product-13.jpg"
+  
+
+    document.getElementById("product_0_2").src = "images/Chaquetas/product-43.jpg"
+    document.getElementById("product_0_3").src = "images/Vestidos/product-2.jpg"
+    document.getElementById("product_0_4").src = "images/Vestidos/product-3.jpg"
+    document.getElementById("product_0_5").src = "images/Chaquetas/product-1.jpg"
+
+
+
+    document.getElementById("product_3").innerHTML = "Vestido Tommy Hilfige"
+    document.getElementById("product_2").innerHTML = "Vestido Karen Miller"
+    document.getElementById("product_1").innerHTML = "Chaqueta Tommy Hilfiger"
+    document.getElementById("product_4").innerHTML = "Chaqueta North Face"
+
+    document.getElementById("price_2").innerHTML = "RD$ 3750"
+    document.getElementById("price_0").innerHTML = "RD$ 7000"
+    document.getElementById("price_1").innerHTML = "RD$ 6050"
+    document.getElementById("price_3").innerHTML = "RD$ 3900"
+    document.getElementById("price_4").innerHTML = "RD$ 5250"
+    };
+   ;
